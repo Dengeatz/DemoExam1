e322c1a3e2f76b6b838a70705d52f3a898deec32
Ilya Losev <gameone.exe@gmail.com>
2024-09-28 13:36:28 +0500

    Added new routers, Added ordersCompleted, Added averageTime, Added brokenParts
    Fix OrderDTO, Modify Order

--- Program.cs
+++ Program.cs
@@ -3,26 +3,30 @@ public class Program
 
     public static void Main(string[] args)
     {
-        int ordersCompleted = 0;
-        bool statusUpdate = false;
-        bool statusComplete = false;
-        string messageComplete = "";
-        string messageUpdate = "";
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
+
+        bool statusUpdate = false;
+        bool statusComplete = false;
+        
+        string messageComplete = "";
+        string messageUpdate = "";
+        
         Repository repository = new();
 
         app.MapGet("/", () => 
         {
             if (statusUpdate || statusComplete) {
-                return Results.Json(new Message(repository.orders, messageUpdate, messageComplete, ordersCompleted));
+                return Results.Json(new Message(repository.orders, messageUpdate, messageComplete));
             }
             else 
             {
                 return Results.Json(repository.orders);
             }
         });
+
         app.MapGet("/{id}", (int id) => repository.orders.Find(o => o.Id == id));
+        
         app.MapGet("/filter/{param}", (string param)
             => repository.orders.FindAll(o =>
                 o.Device == param
@@ -32,7 +36,43 @@ public class Program
                 || o.Status == param
                 || o.Master == param)
         );
+
+        app.MapGet("/ordersCompleted", () => 
+            repository.orders.FindAll(
+                (Order order) => order.Status.Equals("Завершено")).Count 
+        );
+
+        app.MapGet("/averageTime", () => 
+        {  
+            var orders = repository.orders.FindAll((Order order) => order.Status.Equals("Завершено"));
+            double avgTime = 0.0;
+            foreach(var order in orders)
+            {
+                avgTime += order.AverageTime;
+            }
+            return avgTime / orders.Count;
+        }
+        );
+
+        app.MapGet("/brokenParts", () => 
+        {
+            Dictionary<string, int> brokenParts = new();
+            for(int i = 0; i < repository.orders.Count; i++)
+            {
+                if (brokenParts.ContainsKey(repository.orders[i].Problem))
+                {
+                    brokenParts[repository.orders[i].Problem]++;
+                } else
+                {
+                    brokenParts.Add(repository.orders[i].Problem, 1);
+                }
+            }
+            return brokenParts;
+        }
+        );
+
         app.MapPost("/", (Order order) => repository.orders.Add(order));
+        
         app.MapPut("/{id}", (OrderDTO orderDTO, int id) 
             => 
             {
@@ -43,45 +83,46 @@ public class Program
 
                 statusUpdate = true;
                 messageUpdate = "Status updated";
-                if(orderDTO.status == "Complete")
+                if(orderDTO.Status == "Complete")
                 {
                     statusComplete = true;
                     messageComplete = "Status completed";
-                    ordersCompleted++;
+                    order.EndDate = DateTime.Now;
                 }
-                order.Status = orderDTO.status;
-                order.Description = orderDTO.description;
-                order.Master = orderDTO.master;
+                order.Status = orderDTO.Status;
+                order.Description = orderDTO.Description;
+                order.Master = orderDTO.Master;
             }
         );
+        
         app.MapPut("/comment/{id}", (int id, Comment comment) => 
             { 
                 var order = repository.orders.Find(o => o.Id == id);
                 if (order == null)
                     return;
-                Console.WriteLine("Order: " + comment.CommentText);
 
                 order.Comment = comment.CommentText;
             }
         );
+        
         app.Run();
     }
 }
 
 public class Repository
 {
-    public List<Order> orders = [new Order(2, DateTime.Now, "Samsung", "Broken screen", "FIX PLEASE", "J", "Start", "V"), new Order(0, DateTime.Now, "Notebook", "Broken keyboard", "dsadas", "Z", "Start", "O")];
+    public List<Order> orders = [new Order(2, DateTime.Now, "Samsung", "Broken keyboard", "FIX PLEASE", "J", "В ожидании", "V"), new Order(0, DateTime.Now, "Notebook", "Broken keyboard", "dsadas", "Z", "Завершено", "O")];
 }
 
 public class OrderDTO
 {
-    public string status;
-    public string description;
-    public string master;
+    private string? _status;
+    private string? _description;
+    private string? _master;
 
-    public string Status { get => status; set { status = value; } }
-    public string Description { get => description; set { description = value; } }
-    public string Master { get => master; set { master = value; } }
+    public string Status { get => _status; set { _status = value; } }
+    public string Description { get => _description; set { _description = value; } }
+    public string Master { get => _master; set { _master = value; } }
 }
 
 public class Order
@@ -95,6 +136,8 @@ public class Order
     private string _status;
     private string _master;
     private string _comment;
+    private DateTime _endDate;
+    private double _averageTime;
 
     public int Id { get => _id; set => _id = value; }
     public DateTime Date { get => _date; set => _date = value; }
@@ -105,6 +148,8 @@ public class Order
     public string Status { get => _status; set => _status = value; }
     public string Master { get => _master; set => _master = value; }
     public string Comment { get => _comment; set => _comment = value; }
+    public DateTime EndDate { get => _endDate; set => _endDate = value; }
+    public double AverageTime { get => _averageTime = _date.Subtract(_endDate).TotalDays; private set { } }
 
     public Order(int id, DateTime date, string device, string problem, string description, string client, string status, string master)
     {
@@ -119,7 +164,7 @@ public class Order
     }
 }
 
-public record class Message(List<Order> orders, string messageUpdate, string messageComplete, int ordersCompleted);
+public record class Message(List<Order> orders, string messageUpdate, string messageComplete);
 
 public class Comment
 {
